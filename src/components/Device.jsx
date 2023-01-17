6666ecbdfa183be7da1340c85b6048ea060e7436
Måns Mårlid <mans.marlid@zenon.se>
2023-01-17 09:52:06 +0100

    first commit

---
+++ src/components/Device.jsx
@@ -0,0 +1,123 @@
+import BatteryGauge from "react-battery-gauge";
+import {ValueContainer} from "./ValueContainer";
+import React, { useState, useEffect } from "react";
+const url = "http://localhost:3000/devices";
+
+const styles = {
+  wrapper: {
+    width: 1000,
+    boxShadow: "black 10px 5px 5px",
+    backgroundColor: 'white',
+    margin: 20
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  leftHeader: {
+    position: "relative",
+    right: 300,
+  },
+  battery: {
+    position: "relative",
+    left: 300,
+  },
+  values:{
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  online: {
+    color: "green",
+  },
+  offline: {
+    color: "red",
+  },
+};
+
+export default function Device() {
+  const [deviceData, setDeviceData] = useState([]);
+
+  const fetchData = () => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setDeviceData(data);
+
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+    setInterval(() => {
+      fetchData();
+    }, 1000);
+  }, []);
+  return (
+    <>
+      {deviceData.devices == undefined ? (
+        <div>
+         <h1>Loading...</h1>
+        </div>
+      ) : (
+          <div>
+            {deviceData.devices.map((item, i) => (
+                <div key={i} style={styles.wrapper}>
+                  <div style={styles.header}>
+                    <div style={styles.leftHeader}>
+                      <h1>{item.deviceName}</h1>
+                      {item.onlineStatus == 1 ? (
+                          <span style={styles.online}>online</span>
+                      ) : (
+                          <span style={styles.offline}>offline</span>
+                      )}
+                    </div>
+                    <BatteryGauge
+                        customization={{
+                          readingText: {
+                            fontSize: 34,
+                          },
+                        }}
+                        style={styles.battery}
+                        size={60}
+                        value={item.batteryPer}
+                    />
+                  </div>
+                  <div style={styles.values}>
+                    < ValueContainer
+                        title={'eco2'}
+                        value={item.eco2}
+                        unit={'ppm'}
+                        max={3000}
+                    />
+                    < ValueContainer
+                        title={'temperature'}
+                        value={item.ambientTemp}
+                        unit={'°C'}
+                        max={40}
+                    />
+                    < ValueContainer
+                        title={'humidity'}
+                        value={item.humidity}
+                        unit={'%'}
+                        max={100}
+                    />
+                    < ValueContainer
+                        title={'TVOC'}
+                        value={item.tvoc}
+                        unit={'ppb'}
+                        max={861}
+                    />
+                  </div>
+                </div>
+            ))}
+          </div>
+      )
+      }
+    </>
+  );
+}
---
+++ src/components/ValueContainer.jsx
@@ -0,0 +1,24 @@
+import ArcProgress from "react-arc-progress";
+import React from "react";
+const styles = {
+    values: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    }
+}
+export const  ValueContainer = ({title, unit, value, max}) => {
+    const progress = value/max
+    return(   <div style={styles.values}>
+            <div>
+                <ArcProgress
+                    progress={progress}
+                    text= {`${value} ${unit}`}
+                    textStyle={{ size: "30px", font: "tahoma" }}
+                />
+
+                <h2>{title}</h2>
+            </div>
+        </div>
+    )
+}
